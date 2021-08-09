e02ae369324b4bba848c8bdc20f85969c4d58a77
J90-arch <jokubasvirsilas@gmail.com>
2021-08-09 15:16:10 +0300

    expanded pixels.py to search for a horizontal line with most white pixels and find their approximate centre

--- pixels.py
+++ pixels.py
@@ -1,63 +1,33 @@
+import os
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-PATH = "test.jpg"
-img = cv2.imread(PATH, 0)
 ###Code currently is held together by ducktape, so expect it to break
-img = cv2.resize(img, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_CUBIC)
-#img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-kernel = np.ones((1, 1), np.uint8)
-img = cv2.dilate(img, kernel, iterations=1)
-img = cv2.erode(img, kernel, iterations=1)
-img = cv2.GaussianBlur(img, (5, 5), 0)
-#img = cv2.threshold(img, 64, 256, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+def baseline_img(img):
+    img = cv2.resize(img, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_CUBIC)
+    #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+    #img = cv2.threshold(img, 64, 256, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,7) #imgf contains Binary image
-#img = cv2.fastNlMeansDenoising(img, None, 10, 10, 7) 
-#cv2.imshow("img", img)
-#cv2.waitKey()
-#print(img.shape)
-cv2.imwrite("test-mono.jpg", img)
-
-img = np.array(img, int)
-
-
-
-img = np.rot90(img)#rotate image 90 degrees so columns are one list each
-
-#def find_first(x):
-#    idx = x.view(bool).argmax() // x.itemsize
-#    return idx if x[idx] else -1
-
-
-#black_pixels = [[find_first(img[i]), find_first(img[i][::-1])] for i in range(len(img))]
-#for item in black_pixels:
-#    print(item)
-'''
-for i in range(len(img[0])):
-    black_pixel_before = True
-    #j for line, i for column
-    for j in range(len(img)):
-        if img[-j][i] == 0 and black_pixel_before:
-            edges[-j][i] = 255
-            black_pixel_before = False
-        elif img[-j][i] == 255 and not black_pixel_before:
-            black_pixel_before = True
-'''
-# we need to use negative j, because we want to evaluate pixels from bottom to the top
-l = img.shape[1]//15
-print(l)
-
-photo = False
-if photo:
+    img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,7) #imgf contains Binary image
+    #img = cv2.fastNlMeansDenoising(img, None, 10, 10, 7) 
+    cv2.imwrite("test-mono.jpg", img)
+    img = np.array(img, int)
+    img = np.rot90(img)#rotate image 90 degrees so columns are one list each
+    # we need to use negative j, because we want to evaluate pixels from bottom to the top
+    l = img.shape[1]//15
+    print(l)
     edges = np.zeros(img.shape, dtype = int)
     for i in range(len(img)):
         for j in range(l -1, len(img[0])):
             if img[i][-j] == 0 and 0 not in img[i][-j+1:-j+l]:
                 edges[i][-j] = 255
                 j+=l
-       
+        
     edges = np.rot90(edges, 3)
     print(edges.shape)
     #remove single white pixels
@@ -66,46 +36,48 @@ if photo:
             if edges[i][j] == 255:
                 if edges[i][j-1] == 0 and edges[i][j+1] == 0 :
                     edges[i][j] = 0
-    cv2.imwrite('img.png', edges)
+    cv2.imwrite('tmp.png', edges)
+    return edges
 
-else:
-    print(img.shape)
-    base_pixels = [[],[]]
-    for i in range(img.shape[0]-1):
-        for j in range(l -1, img.shape[1]):
-            if img[i][-j] == 0 and 0 not in img[i][-j+1:-j+l]:
-                                
-                base_pixels[0].append(i)#x
-                base_pixels[1].append(j)#y
+# Find a horizontal line with most pixels in middle half of the image
+def find_whitest_line(img):
+    number_of_white_pixels = 0
+    for i in range(len(img)//4,len(img)//4*3):
+        num = np.sum(img[i] == 255)
+        if num >= number_of_white_pixels:
+            number_of_white_pixels = num
+            line_with_most_white_pixels = i
+    return line_with_most_white_pixels
 
-                j=0
-                i+=1
-    
-    for i in range(1, len(base_pixels)-1):
-        if base_pixels[0][i] != base_pixels[0][i-1] +1 and base_pixels[0][i] != base_pixels[0][i+1] -1:
-            base_pixels[0].pop(i)
-            base_pixels[1].pop(i)
-    
-    base_pixels[0].reverse()
-    base_pixels[0] = [img.shape[0]-x for x in base_pixels[0]]
-    base_pixels[1].reverse()
-    plt.plot(base_pixels[0], base_pixels[1], "r+")
+# Find first and last pixel in that line within 1/20 of image length to other white pixels
+def points(whitest_line):
+    for i in range(len(whitest_line)//20 , len(whitest_line)):
+        if whitest_line[i] == 255 and 255 in whitest_line[i:i+len(whitest_line)//20]:
+            First_white_pixel = i
+            break
+    for i in range(First_white_pixel , len(whitest_line)):
+        if 255 not in whitest_line[i:i+len(whitest_line)//20]:
+            Last_white_pixel = i-1
+            break
+    Approx_centre = int((First_white_pixel + Last_white_pixel)/2)
+    return First_white_pixel,Last_white_pixel,Approx_centre
+
+def main():
+    PATH = "test.jpg"
+    img = cv2.imread(PATH, 0)
+    img = baseline_img(img)
+    # rgbimg has to be after baseline_img()
+    rgbimg = cv2.imread('tmp.png')
+    line_with_most_white_pixels = find_whitest_line(img)  
+    First_white_pixel,Last_white_pixel,Approx_centre = points(img[line_with_most_white_pixels])
+    Approx_centre = int((First_white_pixel + Last_white_pixel)/2)
+    print(f'{First_white_pixel= } {Last_white_pixel= } {Approx_centre= }')
+    rgbimg[line_with_most_white_pixels] = [[255,0,0]] * len(rgbimg[0])
+    for i in range(len(rgbimg)):
+        rgbimg[i][Approx_centre] = [255,0,0]
+    plt.imshow(rgbimg,cmap = 'gray')
     plt.show()
+    os.remove('tmp.png')
 
-#print(img.shape)
-#img = np.rot90(img)
-#print(img.shape)
-
-
-#for item in img:
-#    black_pixels = np.where(img == 255)
-#    print(black_pixels)
-#pixels = np.asarray(Image.open(img))
-#print(pixels[0])
-#img = Image.fromarray(np.uint8(pixels))
-
-#cv2.imshow("current",img)
-
-#cv2.waitKey()
-#cv2.imwrite('img.png', img)
-#cv2.imwrite('img.png', np.array([[256, 256], [256, 0]]))
+if __name__ == "__main__":
+    main()
